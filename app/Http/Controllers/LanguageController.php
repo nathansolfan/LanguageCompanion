7f79fac37c6789c76a0f12e4920a98ddf04d82a4
Nathan Ferreira <78206940+nathansolfan@users.noreply.github.com>
2024-08-19 23:44:33 +0100

    created the fetchWordOfTheDay() method using the API

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -3,8 +3,25 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class LanguageController extends Controller
 {
-    //
+    public function index()
+    {
+        // fetch data
+        $wordData = $this->fetchWordOfTheDay();
+
+        return view('welcome', compact('wordData', 'quoteData'));
+    }
+
+    private function fetchWordOfTheDay()
+    {
+        $response = Http::withHeaders([
+            'x-rapidapi-host' => 'wordsapiv1.p.rapidapi.com',
+            'x-rapidapi-key' => env('WORDS_API_KEY')
+        ])->get('https://wordsapiv1.p.rapidapi.com/words/');
+
+        return $response->json();
+    }
 }
